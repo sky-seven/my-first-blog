a19c6509c4e6ed823aa3e8dd8990dc23c3636b8d
Gen <gencau@gmail.com>
2016-04-13 19:14:45 -0400

    My first app commit

---
+++ test.py
@@ -0,0 +1,13 @@
+
+def myFunc():
+    if (3 > 2):
+        print ("hey hey")
+
+myFunc()
+print("C'est fini!")
+
+gang = ['moi', 'isa', 'gen', 'yen','uyen','serge']
+for person in gang:
+    print(person)
+for i in range(1,10):
+    print (i)
